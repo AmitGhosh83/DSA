266e553f5d3b24f3ea1e36d1ffae884d39a4127d
AmitGhosh83 <amitghosh83@gmail.com>
2024-07-28 10:56:18 -0700

    Prepend

--- DoubleLinkedList/DoublyLinkedList.py
+++ DoubleLinkedList/DoublyLinkedList.py
@@ -77,7 +77,18 @@ class DoublyLinkedList:
             self.tail.next = None
         self.length -= 1
         return temp    
-
+    
+    def prepend(self, value):
+        new_node = Node(value)
+        if (self.length == 0 ):
+            self.head = new_node
+            self.tail = new_node
+        else: 
+            new_node.next = self.head
+            self.head.prev = new_node
+            self.head = new_node
+        self.length += 1
+        return True
 
 
 
@@ -85,6 +96,7 @@ my_doubly_linked_list = DoublyLinkedList(5)
 my_doubly_linked_list.append(4)
 my_doubly_linked_list.append(7)
 my_doubly_linked_list.print_list()  
-my_doubly_linked_list.pop()  
+#my_doubly_linked_list.pop()  
+my_doubly_linked_list.prepend(9)
 
 my_doubly_linked_list.print_list()  
