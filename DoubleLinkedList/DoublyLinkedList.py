b26b49b3772bab73fd241549812d9cb3eca86213
AmitGhosh83 <amitghosh83@gmail.com>
2024-07-28 11:19:33 -0700

    Pop_First

--- DoubleLinkedList/DoublyLinkedList.py
+++ DoubleLinkedList/DoublyLinkedList.py
@@ -90,13 +90,37 @@ class DoublyLinkedList:
         self.length += 1
         return True
 
-
+    # def pop_first(self):
+    #     if(self.length == 0):
+    #         return None
+    #     temp = self.head
+    #     self.head = self.head.next
+    #     self.head.prev = None
+    #     temp.next = None
+    #     self.length -= 1
+    #     if(self.length == 0):
+    #         self.tail = None
+    #     return temp;    
+    def pop_first(self):
+        if(self.length ==0):
+            return None
+        temp = self.head
+        if(self.length == 1):
+            self.head = None
+            self.tail = None
+        else:
+            self.head = self.head.next
+            self.head.prev = None
+            temp.next = None
+        self.length -= 1
+        return temp       
 
 my_doubly_linked_list = DoublyLinkedList(5)   
 my_doubly_linked_list.append(4)
 my_doubly_linked_list.append(7)
 my_doubly_linked_list.print_list()  
 #my_doubly_linked_list.pop()  
-my_doubly_linked_list.prepend(9)
+#my_doubly_linked_list.prepend(9)
+my_doubly_linked_list.pop_first()
 
 my_doubly_linked_list.print_list()  
