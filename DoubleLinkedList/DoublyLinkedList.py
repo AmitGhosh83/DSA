6791115de74aa08ef321828b716d645fa6a6a962
AmitGhosh83 <amitghosh83@gmail.com>
2024-07-28 10:36:05 -0700

    Pop

--- DoubleLinkedList/DoublyLinkedList.py
+++ DoubleLinkedList/DoublyLinkedList.py
@@ -34,7 +34,57 @@ class DoublyLinkedList:
             self.length += 1 
         return True         
 
+    # Using Linked LIst logic, Time Complexity O(n)
+    # def pop(self):
+    #     if self.length == 0:
+    #         return None
+    #     pre = temp = self.head
+    #     while(temp.next):
+    #         pre = temp
+    #         temp = temp.next
+    #     self.tail = pre
+    #     self.tail.next = None
+    #     temp.prev = None
+    #     self.length -= 1
+    #     if(self.length == 0):
+    #         self.tail = None
+    #     return temp              
+
+    # def pop(self):
+    #     if self.length == 0:
+    #         return None
+    #     temp = self.tail
+    #     self.tail = self.tail.prev
+    #     self.tail.next = None
+    #     temp.prev = None
+    #     self.length -=1
+    #     if(self.length == 0): 
+    #         self.head = None
+    #         self.tail = None
+    #     return temp
+    
+    # Time comlexity O(1), using prev pointer
+    def pop(self):
+        if(self.length ==0):
+            return None
+        temp = self.tail
+        if(self.length == 1):
+            self.head = None
+            self.tail = None
+        else:
+            self.tail = self.tail.prev
+            temp.prev = None
+            self.tail.next = None
+        self.length -= 1
+        return temp    
+
+
+
+
 my_doubly_linked_list = DoublyLinkedList(5)   
-my_doubly_linked_list.print_list()   
 my_doubly_linked_list.append(4)
-my_doubly_linked_list.print_list()         
+my_doubly_linked_list.append(7)
+my_doubly_linked_list.print_list()  
+my_doubly_linked_list.pop()  
+
+my_doubly_linked_list.print_list()  
