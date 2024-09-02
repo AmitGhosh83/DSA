f3b3e2fefcb6872b79795e898d565186454c62fe
Amit Kumar Ghosh <Amit_kumar_Ghosh@hotmail.com>
2024-09-02 13:26:58 -0700

    Delete with min value helper function and comments

--- BinarySearchTree/BSTRecursion.py
+++ BinarySearchTree/BSTRecursion.py
@@ -84,7 +84,44 @@ class BinarySearchTree:
             current_node.right = self.__r_insert(current_node.right, value)
         # This first returns the newly created node to the last leaf node and subsequently the pointer to the previous recurvise call ensuring its call to current_node.left/right is pointing to the right child.    
         return current_node
-        
+
+
+    def r_delete(self, value):
+        self.__r_delete(self.root, value)
+
+    def __r_delete(self, current_node, value):
+        if current_node == None:
+            return False
+        if value < current_node.value:
+            current_node.left = self.__r_delete(current_node.left, value)
+        elif value > current_node.value:
+            current_node.right = self.__r_delete(current_node.right, value)
+        # When there is a match and current_node is pointing to the node to be deleted, 4 scenarios can happen
+        else:
+            # Case 1: When current_node is the  leaf node, we return None to the caller
+            if current_node.left == None and current_node.right == None:
+                return None
+            # Case 2: When left child is Empty, we make current_node equal to current_node.right and return that to caller
+            elif current_node.left == None:
+                current_node = current_node.right
+            # Case 3: When right child is empty, we make current_node equal to current_node.left and return that to caller    
+            elif current_node.right == None:
+                current_node = current_node.left  
+            # Case 4: 
+                # 1. There current node has a subtree and we need to traverse to the right to find the lowest value of the subtree, use min_value to return that value  
+                # 2. Update current_node value to min value of the current nodes right subtree
+                # 3. Delete the right subtree leaf node    
+            else:
+                right_sub_tree_min = self.min_value(current_node.right)
+                current_node.value = right_sub_tree_min
+                current_node.right = self.__r_delete(current_node.right, right_sub_tree_min)
+        return current_node    
+    
+    def min_value(self, current_node):
+        while current_node.left is not None:
+            current_node = current_node.left
+        return current_node.value    
+
 
 
 
@@ -109,9 +146,19 @@ my_BST.r_insert(2)
 my_BST.r_insert(1)
 my_BST.r_insert(3)
 
-
+print("root =", my_BST.root.value)
+print("root.left =", my_BST.root.left.value)
+print("root.right =", my_BST.root.right.value)
 #print(my_BST.contains(82))
-print(my_BST.r_contains(3))
+#print(my_BST.r_contains(3))
+
+print("\nAfter Delete\n")
+
+my_BST.r_delete(3)
+
+print("root =", my_BST.root.value)
+print("root.left =", my_BST.root.left.value)
+print("root.right =", my_BST.root.right)
 # print(my_BST.contains(1099))
 # print(my_BST.contains(1))
 # print(my_BST.contains(17))
