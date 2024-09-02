12f309274c07360c0a134f1718ac5476c9e7d2f4
Amit Kumar Ghosh <Amit_kumar_Ghosh@hotmail.com>
2024-09-02 10:14:33 -0700

    Comments for recursive Contains and Insert

--- BinarySearchTree/BSTRecursion.py
+++ BinarySearchTree/BSTRecursion.py
@@ -45,7 +45,9 @@ class BinarySearchTree:
     
     def r_contains(self, value):
         return self.__r_contains(self.root, value)
-
+    # self.root is the current node in __r_contains
+    # Notice the self on __r_contains, that is  called on itself.
+    
     def __r_contains(self, current_node, value):
         if current_node == None:
             return False
@@ -53,22 +55,38 @@ class BinarySearchTree:
             return True
         if value < current_node.value:
             return self.__r_contains(current_node.left, value)
-        if value > current_node.value:
+        # Notice the self, its called on itself
+        elif value > current_node.value:
             return self.__r_contains(current_node.right, value)
+        # function cant be called without the object instance, notice self
+
 
     def r_insert(self, value):
-        if self.root == None: 
+        if self.root == None:
             self.root = Node(value)
         self.__r_insert(self.root, value)
 
+        # 1. Starts at self.root and checks for value, if same just return current node which is self.root, that is ignores duplicate value 
+        # 2. If value to be inserted is less that self.root value, points the self.root left (left child) to make another check on left child's child. This is done recursively
+        # 3.This goes on till, the left of the right child point to None, that is end of the tree. Thats the base case
+        # 4. When base is reached, current_node == None becomes true, thats when you create the node
+        # 5. Once node is created, we fall on the return current node, which returns the newly created current_node to the last recursive call
+        # 6. The trick here is the parent, current_node.left\ right is now maintaing the link to the newly created current_node
+        # 7. The call stack unravels till that chain comes all the way to the self.root
+
     def __r_insert(self, current_node, value):
+        # base case for the node to be created
         if current_node == None:
             return Node(value)
         if value < current_node.value:
             current_node.left = self.__r_insert(current_node.left, value)
         if value > current_node.value:
             current_node.right = self.__r_insert(current_node.right, value)
-        return current_node        
+        # This first returns the newly created node to the last leaf node and subsequently the pointer to the previous recurvise call ensuring its call to current_node.left/right is pointing to the right child.    
+        return current_node
+        
+
+
 
     
 
